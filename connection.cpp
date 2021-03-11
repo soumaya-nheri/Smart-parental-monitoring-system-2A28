7f5710d65b57512f85e1a95a835e6fc3f72180b1
Oumayma <oumayma.bahroun@esprit.tn>
2021-03-11 13:13:14 +0100

    test 1 tuto git

--- connection.cpp
+++ connection.cpp
@@ -1,5 +1,5 @@
 #include "connection.h"
-
+//test tuto git
 Connection::Connection()
 {
 
@@ -8,9 +8,9 @@ Connection::Connection()
 bool Connection::createconnect()
 {bool test=false;
 QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-db.setDatabaseName("");//inserer le nom de la source de données ODBC
-db.setUserName("");//inserer nom de l'utilisateur
-db.setPassword("");//inserer mot de passe de cet utilisateur
+db.setDatabaseName("projet_2a");//inserer le nom de la source de données ODBC
+db.setUserName("oumayma");//inserer nom de l'utilisateur
+db.setPassword("admin");//inserer mot de passe de cet utilisateur
 
 if (db.open())
 test=true;
